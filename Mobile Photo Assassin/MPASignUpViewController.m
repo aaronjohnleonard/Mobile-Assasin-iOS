70a320edb2b18e8c235dfd17ca65fc137f94948f
Caleb Call <jcalebcall@gmail.com>
2014-02-21 16:24:56 -0700

    Create Account functionality added!

--- Mobile Photo Assassin/MPASignUpViewController.m
+++ Mobile Photo Assassin/MPASignUpViewController.m
@@ -61,16 +61,79 @@
         return;
     }
     
-    NSString *post = [NSString stringWithFormat:@"username=%@&password=%@&firstname=%@&lastname=%@&email=%@", self.userName.text, self.password.text, self.firstName.text, self.lastName.text, self.email.text];
-    NSData *postData = [post dataUsingEncoding:NSUTF8StringEncoding];
-    NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
+    if([self.userName.text isEqualToString:@""] ||
+        [self.password.text isEqualToString:@""] ||
+        [self.firstName.text isEqualToString:@""] ||
+        [self.lastName.text isEqualToString:@""] ||
+        [self.email.text isEqualToString:@""]){
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@""
+                                                        message:@"Please fill in all information"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alert show];
+        return;
+    }
     
+    // Dictionary that holds post parameters. You can set your post parameters that your server accepts or programmed to accept.
+    NSMutableDictionary* _params = [[NSMutableDictionary alloc] init];
+    [_params setObject:self.userName.text forKey:@"username"];
+    [_params setObject:self.password.text forKey:@"password"];
+    [_params setObject:self.firstName.text forKey:@"firstName"];
+    [_params setObject:self.lastName.text forKey:@"lastName"];
+    [_params setObject:self.email.text forKey:@"email"];
+    
+    // the boundary string : a random string, that will not repeat in post data, to separate post data fields.
+    NSString *BoundaryConstant = @"V2ymHFg03ehbqgZCaKO6jy";
+    
+    // string constant for the post parameter 'file'. My server uses this name: `file`. Your's may differ
+    NSString* FileParamConstant = @"photo";
+    
+    // the server url to which the image (or the media) is uploaded. Use your server url here
+    NSURL* requestURL = [NSURL URLWithString:@"http://54.200.120.14:8080/member/createAccount"];
+    
+    // create request
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-    [request setURL:[NSURL URLWithString:@"http://54.200.120.14:8080/member/createAccount"]];
+    [request setCachePolicy:NSURLRequestReloadIgnoringLocalCacheData];
+    [request setHTTPShouldHandleCookies:NO];
+    [request setTimeoutInterval:30];
     [request setHTTPMethod:@"POST"];
-    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    
+    // set Content-Type in HTTP header
+    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", BoundaryConstant];
+    [request setValue:contentType forHTTPHeaderField: @"Content-Type"];
+    
+    // post body
+    NSMutableData *body = [NSMutableData data];
+    
+    // add params (all params are strings)
+    for (NSString *param in _params) {
+        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", BoundaryConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", param] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"%@\r\n", [_params objectForKey:param]] dataUsingEncoding:NSUTF8StringEncoding]];
+    }
+    
+    // add image data
+    NSData *imageData = UIImageJPEGRepresentation(self.imageView.image, 1.0);
+    if (imageData) {
+        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", BoundaryConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"; filename=\"image.jpg\"\r\n", FileParamConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[@"Content-Type: image/jpeg\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:imageData];
+        [body appendData:[[NSString stringWithFormat:@"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+    }
+    
+    [body appendData:[[NSString stringWithFormat:@"--%@--\r\n", BoundaryConstant] dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    // setting the body of the post to the reqeust
+    [request setHTTPBody:body];
+    
+    // set the content-length
+    NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[body length]];
     [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
-    request.HTTPBody = postData;
+    
+    // set URL
+    [request setURL:requestURL];
     
     NSData *urlData;
     NSURLResponse *response;
@@ -81,14 +144,14 @@
                                     returningResponse:&response
                                                 error:&error];
     
-    NSLog(@"%@", [[NSString alloc] initWithData:urlData encoding:NSUTF8StringEncoding]);
+    NSString *replyString = [[NSString alloc] initWithData:urlData encoding:NSUTF8StringEncoding];
     
-    NSDictionary *info = [NSJSONSerialization JSONObjectWithData:urlData options:0 error:nil];
+    NSLog(@"%@", replyString);
     
     // If the json doesn't have user information, send alert to user
-    if (info == nil) {
+    if (![replyString isEqualToString:@"Account created successfully"]) {
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@""
-                                                        message:@"Could not create account"
+                                                        message:replyString
                                                        delegate:nil
                                               cancelButtonTitle:@"OK"
                                               otherButtonTitles:nil];
@@ -97,7 +160,7 @@
     }
     else{
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@""
-                                                        message:@"Account successfully created!"
+                                                        message:@"Account successfully created"
                                                        delegate:nil
                                               cancelButtonTitle:@"OK"
                                               otherButtonTitles:nil];
