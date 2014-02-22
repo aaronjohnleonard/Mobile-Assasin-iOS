4a600184a22b1419d5e27c90b89c9d42a27f5ab9
Kris Alder <krisalder@gmail.com>
2014-02-22 12:47:11 -0700

    started on game creation screen

---
+++ Mobile Photo Assassin/MPAGameCreatorController.h
@@ -0,0 +1,13 @@
+//
+//  MPAGameCreatorController.h
+//  Mobile Photo Assassin
+//
+//  Created by Kris Alder on 14.02.22.
+//  Copyright (c) 2014 Aaron Leonard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPAGameCreatorController : UIViewController
+
+@end
